87d75946660a2a038293c1545ccb276a66223119
Avnz05 <avaneepwaikar@gmail.com>
2025-12-21 23:23:17 +0530

    Changes to be committed: modified:   tsconfig.node.tsbuildinfo 	modified:   vite.config.js 	modified:   vite.config.ts

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
+    base: '/', // or '/SmartAxis/' if deployed to github.io/SmartAxis/
     plugins: [react()],
-    base: "/"
 });
